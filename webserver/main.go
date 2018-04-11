f8b23ad89707679d14bf3e50c8976e54d11fb38c
liwd <13260582198@163.com>
2018-04-11 20:48:45 +0800

    交叉编译

--- webserver/main.go
+++ webserver/main.go
@@ -7,7 +7,7 @@ import (
 
 func firstPage(w http.ResponseWriter, r *http.Request) {
 
-	io.WriteString(w, "<h1>Hello,this is my first page</h1>")
+	io.WriteString(w, "<h1>Hello,this is my deploy page2</h1>")
 }
 
 func main() {
